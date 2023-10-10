3bbe290a2a3dcc90de9c5cd4f03bdc847dd63261
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-10-10 13:27:30 -0400

    code

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Lyver
+ * Created on: Oct 2023
+ * This program plays rock paper scissors
 */
 
-basic.showString('Hello, World!')
+let randomNumber: number = 0
+
+input.onGesture(Gesture.Shake, function () {
+randomNumber = randint(0, 2)
+
+// if randomNumber was 0
+if (randomNumber == 0) {
+  basic.showIcon(IconNames.Scissors)
+}
+
+// if randomNumber was 1 
+if (randomNumber == 1) {
+  basic.showLeds(`
+. . . . .
+. # # # .
+. # # # .
+. # # # .
+. . . . .
+`)
+}
+
+// randomNumber was 2
+})
